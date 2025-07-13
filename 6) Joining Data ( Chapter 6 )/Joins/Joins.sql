247d553f410ca1e3d5128a4deac0092eb6bb8572
Tushar Mahajan <tusharmahajan080@gmail.com>
2025-07-13 23:56:00 +0530

    Basic Joins completed

--- 6) Joining Data ( Chapter 6 )/Joins/Joins.sql
+++ 6) Joining Data ( Chapter 6 )/Joins/Joins.sql
@@ -9,9 +9,9 @@ JOINS
 6) Cross Join 
 
 ## When to use Joins 
-a) Recombine Data   -> Inner , Left
+a) Recombine Data   -> Inner , Left , Full
 b) Data Enrichment "Getting Extra Data" -> Left
-c) Check for existance "Filtering the Data" -> Inner , Left + where
+c) Check for existance "Filtering the Data" -> Inner , Left + where , Full + where
 */
 
 
@@ -25,7 +25,7 @@ SELECT * FROM orders;
 
 
 
--- (b) Inner Join 
+-- (b) Inner Join -> only the matching from A and B
 --Q) Get all customers along with there orders but only those who have placed an order
 SELECT 
 customers.id ,
@@ -46,7 +46,7 @@ ON customers.id=orders.customer_id
 
 
 
--- c) LEFT JOIN
+-- c) LEFT JOIN  -> Everything from left but only the matching from the right
 --Q) Get all customers with their orders along with those who don't have any order 
 SELECT 
 customers.id ,
@@ -57,4 +57,47 @@ orders.order_id,
 orders.order_date
 FROM customers
 LEFT JOIN orders
+ON customers.id = orders.customer_id
+
+
+
+--d) RIGHT JOIN -> Everything from right but only the matching from the left 
+--Q) Get all customers with their orders ,including orders without maching orders 
+SELECT 
+orders.order_id,
+orders.customer_id,
+orders.order_date,
+orders.sales,
+customers.first_name,
+customers.country
+FROM customers
+RIGHT JOIN orders
+ON customers.id = orders.customer_id
+
+
+--Q) Get all the customers along with their orders , including orders without matching their customers (Using Left Join) 
+SELECT
+orders.order_id,
+orders.order_date,
+orders.sales,
+orders.customer_id,
+customers.first_name,
+customers.country
+FROM orders
+LEFT JOIN customers
+ON customers.id = orders.customer_id
+
+
+-- e) FULL OUTER JOIN -> Return all rows from both the tables 
+--Q) Get all customers and all orders even if there is no match 
+SELECT 
+customers.id ,
+customers.first_name,
+customers.country,
+customers.score,
+orders.order_id,
+orders.order_date,
+orders.sales
+FROM customers
+FULL OUTER JOIN orders
 ON customers.id = orders.customer_id
